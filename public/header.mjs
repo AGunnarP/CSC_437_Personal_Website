65aa0275d6c5744e4904dc1971a0bae62770e413
root <root@syn-2600-6c52-7a00-b100-85c3-d834-92cb-588c.biz6.spectrum.com>
2025-05-05 23:46:31 -0700

    Stuff Done

--- public/header.mjs
+++ public/header.mjs
@@ -1,8 +1,29 @@
 const button = document.getElementById("menu_button");
 const links_container = document.getElementById("Links_Container");
 const main = document.getElementsByTagName("main")[0];
+const body = document.getElementsByTagName("body")[0];
+const toggle = document.getElementById("Light_Mode_Toggle");
 
 
+class ThemeController extends HTMLElement {
+    connectedCallback() {
+
+        console.log("Calling back!");
+        const isDark = localStorage.getItem('dark_mode') === 'true';
+        if(isDark)
+            document.body.className = "";
+        else
+            document.body.className = "alternate_body";
+
+        if(isDark && toggle)
+            toggle.checked = false;
+        else
+            toggle.checked = true;
+
+    }
+  }
+  
+  customElements.define('theme-controller', ThemeController);
 
 button.addEventListener("click", () => {
     
@@ -15,9 +36,29 @@ button.addEventListener("click", () => {
 
 main.addEventListener("click", () => {
 
-    console.log("Main was clicked");
-
     if(links_container.className === "toggled_Links_Container")
         links_container.className = "Links_Container";
 
-  });
+});
+
+if(toggle)
+    toggle.addEventListener("click", () => {
+
+        console.log("Toggle was clicked!");
+
+        if(body.className === "alternate_body"){
+
+            body.className = "";
+            localStorage.setItem("dark_mode", "true");
+
+        }
+            
+        else{
+
+            body.className = "alternate_body";
+            localStorage.setItem("dark_mode", "false");
+        }
+
+    })
+
+
